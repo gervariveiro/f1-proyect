bf524002110bb63c8f8c539baf89c9956b8c5aae
gervariveiro <84820639+gervariveiro@users.noreply.github.com>
2022-03-07 11:40:19 -0300

    1st comm

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home () {
+
+    return (
+            
+           <div className='mainhome'>
+                Hola soy Main
+           </div>
+    
+    )
+    }
+    
+export default Home;
---
+++ src/pages/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Login () {
+
+    return (
+            
+           <div className='mainhome'>
+                Hola soy Login
+           </div>
+    
+    )
+    }
+    
+export default Login;
