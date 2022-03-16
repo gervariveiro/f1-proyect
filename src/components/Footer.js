4fb35316679e596b51e25bee5a7d3863973212c2
gervariveiro <gervasioriveiro@gmail.com>
2022-03-16 10:43:45 -0300

    Home page con estilos

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer () {
+
+    return (
+
+
+        <div className='footer'>
+            @Gerva Riveiro 2022
+        </div>
+          
+    )}
+export default Footer;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar.js';
+import Footer from '../components/Footer.js';
 import Portada from '../assets/portadafoto.jpg';
 import f12022 from '../assets/f12022.jpg';
 import Alonso from '../assets/alonso.jpg';
-import F12022_video from '../components/f12022_video';
+
 
 
 function Home () {
@@ -18,29 +19,35 @@ function Home () {
                     <img className='portadafoto' src={Portada}/>
                     <p>Sains en Bahréin</p>
                     </a>
-                    <h5>"No somos favoritos", dice Binotto, pero el F1-75 
+                    <h4>"No somos favoritos", dice Binotto, pero el F1-75 
                         apunta hacia arriba ante el GP de Bahréin. 
-                        El madrileño, en su mejor momento como piloto.</h5>
+                        El madrileño, en su mejor momento como piloto.</h4>
                </section>
                <ul className='videoslista'>
                    <li>
+                       <h4>Grilla de Conductores</h4>
                    <iframe width="560" height="315" src="https://www.youtube.com/embed/QLUA_kYbWuw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                   </li>
+                   <li>
+                       <h4>El cambio de concepto de Mercedes...</h4>
+                       <iframe width="560" height="315" src="https://www.youtube.com/embed/MdAIMLV0jVM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                    </li>
                 </ul>
                <ul className='noticias'>
                    <li>
                        <a href='/formula2022'>
+                            <h4>La revolución del siglo en la F1</h4>
                             <img className='f12022foto' src={f12022}/>
-                            <h5>La revolución del siglo en la F1</h5>
                         </a>
                    </li>
                    <li>
                        <a href='/alonso'>
+                            <h4>El hito que igualará Alonso esta temporada</h4>
                             <img className='alonsofoto' src={Alonso}/>
-                            <h5>El hito que igualará Alonso esta temporada</h5>
                         </a>
                    </li>
                </ul>
+               <Footer/>
            </div>
     
     )
