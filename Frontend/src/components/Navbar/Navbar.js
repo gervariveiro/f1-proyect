cf3af1c5e6c909bf13a3066523924ad5401494ba
gervariveiro <gervasioriveiro@gmail.com>
2022-04-07 10:38:27 -0300

    commit MUI NAvbar color

--- Frontend/src/components/Navbar/Navbar.js
+++ Frontend/src/components/Navbar/Navbar.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import ButtonHome from '../Buttons/Buttonhome';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faBars} from '@fortawesome/free-solid-svg-icons';
+import {AppBar, ThemeProvider, Toolbar, Typography} from '@material-ui/core';
 
 
-/*const btnMenu = document.querySelector("#btnMenu");
-const menu= document.querySelector("#menu");
-btnMenu.addEventListener("click", function(){
-    menu.classList.toggle("mostrar");
-});*/
+
 
 function Navbar () {
     
@@ -16,31 +10,14 @@ function Navbar () {
     return (
 
 
-        <div className='navbar'>
-            <span class="nav-bar">
-            <ButtonHome/>
-            <div className="menuicon" id="btnMenu">
-                <FontAwesomeIcon icon={faBars} />
-            </div>            
-        </span>
-        <nav class="main-nav">
-            <ul class="menu" id="menu">
-                <ButtonHome/>
-                <li class="menu__item">
-                    <a href="" class="menu__link">Equipos</a>
-                </li>
-                <li class="menu__item">
-                    <a href="/drivers" class="menu__link">Conductores</a>
-                </li>
-                <li class="menu__item">
-                    <a href="/tracks" class="menu__link">Circuitos</a>
-                </li>
-                <li class="menu__item">
-                    <a href="/contacto" class="menu__link">Contacto</a>
-                </li>
-            </ul>
-        </nav>
-        <script src="menu.js"></script>
+        <div>
+            <AppBar style={{ backgroundColor: '#FC2D1C' }}>
+                <Toolbar>
+                    <Typography variant='h6'>
+                        F1
+                    </Typography>
+                </Toolbar>
+            </AppBar>
         </div>
     
     )
