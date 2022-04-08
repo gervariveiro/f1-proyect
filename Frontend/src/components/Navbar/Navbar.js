b7731891679c151bf8af2a50f367e36f59dc31c7
gervariveiro <gervasioriveiro@gmail.com>
2022-04-08 10:10:54 -0300

    Menu for Navbar and Button

--- Frontend/src/components/Navbar/Navbar.js
+++ Frontend/src/components/Navbar/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {AppBar, ThemeProvider, Toolbar, Typography} from '@material-ui/core';
-
+import {AppBar, Button, ThemeProvider, Toolbar, Typography} from '@material-ui/core';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBars} from '@fortawesome/free-solid-svg-icons';
+import ButtonHome from '../Buttons/Buttonhome';
 
 
 
 function Navbar () {
-    
 
     return (
 
@@ -13,9 +14,24 @@ function Navbar () {
         <div>
             <AppBar style={{ backgroundColor: '#FC2D1C' }}>
                 <Toolbar>
-                    <Typography variant='h6'>
-                        F1
-                    </Typography>
+                    <ButtonHome/>
+                    <Button id='btnmenu'>
+                        <FontAwesomeIcon icon={faBars} className='menu_icon' id="btnMenu"/>
+                    </Button>
+                    <ul class="menu" id="menu">
+                        <li class="menu__item">
+                            <a href="" class="menu__link">Equipos</a>
+                        </li>
+                        <li class="menu__item">
+                            <a href="/drivers" class="menu__link">Conductores</a>
+                        </li>
+                        <li class="menu__item">
+                            <a href="/tracks" class="menu__link">Circuitos</a>
+                        </li>
+                        <li class="menu__item">
+                            <a href="/contacto" class="menu__link">Contacto</a>
+                        </li>
+                    </ul>
                 </Toolbar>
             </AppBar>
         </div>
