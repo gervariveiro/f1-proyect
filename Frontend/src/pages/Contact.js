7652a025da86a930f18abf3338dbba50470bd50b
gervariveiro <gervasioriveiro@gmail.com>
2022-04-21 11:22:51 -0300

    Estilo form de contacto

--- Frontend/src/pages/Contact.js
+++ Frontend/src/pages/Contact.js
@@ -9,21 +9,23 @@ function Contact () {
            <div className='maincontact'>
                <Navbar/>
                 <div className='driversespacio'></div>
-                <form className='formcontact'>
+                <div className='divform'>
+                <form className='formcontact' action="http://localhost:8080/send-email" method="POST">
                     <label>
                         Name:
-                        <input type="text" name="Nombre" />
+                        <input type="text" name="name" placeholder='Nombre' />
                     </label>
                     <label>
                         Email:
-                        <input type="text" name="Email" />
+                        <input type="text" name="email" placeholder='Email'/>
                     </label>
                     <label>
                         Mensaje:
-                        <input className='mensaje' type="text" name="Escriba un mensaje" />
+                        <input className='mensaje' type="text" name="message" placeholder='Escriba un mensaje' />
                     </label>
-                    <input type="submit" value="Enviar" />
+                    <input className='botonenviar' type="submit" value="Enviar" />
                 </form>
+                </div>
                 <Footer/>
            </div>
             
